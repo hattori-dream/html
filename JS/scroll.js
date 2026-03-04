14d7e4575c314b2bd7c90cd2f99c59e68ab970a6
hattori-dream <hattori.dream@gmail.com>
2026-03-04 16:46:10 +0900

    0304

--- JS/scroll.js
+++ JS/scroll.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
   var splide = new Splide(".splide", {
-    type: "slide", // スライド形式（'loop'にすると無限ループ）
-    perPage: 1, // 1画面に表示する枚数
-    arrows: false, // 矢印ボタンを表示
-    pagination: true, // ページネーション（ドット）を表示
-    wheel: true, // マウスホイールでの操作を有効化
-    releaseWheel: true, // スライダーの端まで行ったら通常のスクロールに戻る
-    speed: 500, // スライド速度
+    type: "slide",
+    perPage: 1,
+    arrows: false,
+    pagination: true,
+    wheel: true,
+    releaseWheel: true,
+    speed: 500,
   });
 
   splide.mount();
